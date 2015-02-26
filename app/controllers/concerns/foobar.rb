b2f2a2e134a33a5fc2a186cc0548ed32feae3ab7
Junhyung Kim <kimjunh@berkeley.edu>
2015-02-25 18:16:53 -0800

    initial commit

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,2 +1,14 @@
 class Foobar
+  def self.baz(arr)
+  	sum = 0
+  	duplicate = []
+  	for str_int in arr
+  	  int_str = str_int.to_i + 2
+  	  if not duplicate.include?(int_str) and int_str % 2 == 0 and int_str <= 10
+  	  	duplicate << int_str
+  	  	sum += int_str
+  	  end
+  	end
+  	sum
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,20 @@
 class PagesController < ApplicationController
   def home
-    # @baz = Foobar.baz ["1", "2", "3", "3", "4", "5", "10", "11", "100"]
+    @baz = Foobar.baz ["1", "2", "3", "3", "4", "5", "10", "11", "100"]
   end
 
   def stringify
-    @text = "You are nothing!"
+    if params[:name].blank?
+      @text = "You are nothing!"
+    else
+      @text = params[:name] + " " + params[:adjective]
+    end
   end
 
   def age
   end
 
   def person
+    @me = Person.new params[:name], params[:age]
   end
 end
